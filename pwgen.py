3f0a4cec9efeb5d1f98e3f0c32277d79b1fc0ab4
Eskil Reiremo <39136462+eskrei01@users.noreply.github.com>
2020-08-05 13:13:05 +0200

    Changed formatting

--- pwgen.py
+++ pwgen.py
@@ -1,5 +1,4 @@
-import random
-import string
+import random, string
 
 #This function generates the password
 def pwgen(length):
@@ -9,7 +8,15 @@ def pwgen(length):
 #Ask user for input
 quantity=int(input("How many passwords do you want? : "))
 length=int(input("How many characters do you want? (15+): "))
-complexity=int(input("Choose which complexity you want: \n1: Letters, Punctuation and Digits \n2: Letters and Punctuation \n3: Punctuation and Digits \n4: Letters and Digits \n5: Punctuation \n6: Letters \n7: Digits \n: "))
+complexity=int(input((("Choose which complexity you want: \n" 
+    "1: Letters, Punctuation and Digits \n" 
+    "2: Letters and Punctuation \n" 
+    "3: Punctuation and Digits \n" 
+    "4: Letters and Digits \n" 
+    "5: Punctuation \n" 
+    "6: Letters \n" 
+    "7: Digits \n" 
+    ": "))))
 
 if length >= 15: #Demands password of at least 15 characters
     for i in range(quantity): #Generates password from user choices
@@ -17,7 +24,7 @@ if length >= 15: #Demands password of at least 15 characters
             chars = string.ascii_letters + string.punctuation + string.digits
             pwgen(length)
             
-        elif complexity == 2:
+        elif complexity == 2:   
             chars = string.ascii_letters + string.punctuation
             pwgen(length)
 

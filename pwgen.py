5daa07eb737cdeb428f1a5241e04bcd5a29e48fd
Eskil Reiremo <39136462+eskrei01@users.noreply.github.com>
2020-08-06 10:36:33 +0200

    Dictionary test
    Added check to see if duplicates are created

--- pwgen.py
+++ pwgen.py
@@ -2,8 +2,19 @@ import random, string
 
 #This function generates the password
 def pwgen(length):
-    result_str = ''.join(random.choice(chars) for i in range(length))
-    print("Your random password is:", result_str)
+    keyval = 2
+    for q in range(quantity): #Generates password from user choices
+        result_str = ''.join(random.choice(chars) for i in range(length))
+        ini_dict = {keyval:result_str}
+        print("Your random password is:", result_str)
+        keyval = keyval + 1
+    rev_dict = {}
+    for key, value in ini_dict.items(): 
+        rev_dict.setdefault(value, set()).add(key) 
+
+    result = [key for key, values in rev_dict.items() 
+                                  if len(values) > 1]
+    print("duplicate values", str(result)) 
 
 #Ask user for input
 quantity=int(input("How many passwords do you want? : "))
@@ -19,36 +30,35 @@ complexity=int(input((("Choose which complexity you want: \n"
     ": "))))
 
 if length >= 15: #Demands password of at least 15 characters
-    for i in range(quantity): #Generates password from user choices
-        if complexity == 1:
-            chars = string.ascii_letters + string.punctuation + string.digits
-            pwgen(length)
+    if complexity == 1:
+        chars = string.ascii_letters + string.punctuation + string.digits
+        pwgen(length)
             
-        elif complexity == 2:   
-            chars = string.ascii_letters + string.punctuation
-            pwgen(length)
+    elif complexity == 2:   
+        chars = string.ascii_letters + string.punctuation
+        pwgen(length)
 
-        elif complexity == 3:
-            chars = string.punctuation + string.digits
-            pwgen(length)
+    elif complexity == 3:
+        chars = string.punctuation + string.digits
+        pwgen(length)
 
-        elif complexity == 4:
-            chars = string.ascii_letters + string.digits
-            pwgen(length)
+    elif complexity == 4:
+        chars = string.ascii_letters + string.digits
+        pwgen(length)
 
-        elif complexity == 5:
-            chars = string.punctuation
-            pwgen(length)
+    elif complexity == 5:
+        chars = string.punctuation
+        pwgen(length)
 
-        elif complexity == 6:
-            chars = string.ascii_letters
-            pwgen(length)
+    elif complexity == 6:
+        chars = string.ascii_letters
+        pwgen(length)
 
-        elif complexity == 7:
-            chars = string.digits
-            pwgen(length)
+    elif complexity == 7:
+        chars = string.digits
+        pwgen(length)
+
     input("Press enter to exit: ")
-            
-else:
-    print("You must create a password with at least 15 characters")
-    input("Press enter to exit: ")
+
+elif length >= 15:
+    
